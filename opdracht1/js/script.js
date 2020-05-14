57d94c09a7daccfeaa860f0d65802c9cc02eefe5
Lisa Oude Elferink <lisaoudeelferink@hotmail.com>
2020-05-14 15:31:48 +0200

    opdracht 1, versie 2

--- opdracht1/js/script.js
+++ opdracht1/js/script.js
@@ -5,10 +5,6 @@
 // VASTE VARIABELEN
 var knopRood = document.querySelector('#knoprood');
 var knopOranje = document.querySelector('#knoporanje');
-var knopGroen = document.querySelector('#knopgroen');
-var knopBlauw = document.querySelector('#knopblauw');
-var knopRoze = document.querySelector('#knoproze');
-var knopPaars = document.querySelector('#knoppaars');
 
 
 var rood1 = document.querySelector('.rood1');
@@ -18,23 +14,40 @@ var rood4 = document.querySelector('.rood4');
 var rood5 = document.querySelector('.rood5');
 var rood6 = document.querySelector('.rood6');
 
-var oranje1 = document.querySelector('.oranje1')
-var oranje2 = document.querySelector('.oranje2')
-var oranje3 = document.querySelector('.oranje3')
-var oranje4 = document.querySelector('.oranje4')
-var oranje5 = document.querySelector('.oranje5')
-var oranje6 = document.querySelector('.oranje6')
+var oranje1 = document.querySelector('.oranje1');
+var oranje2 = document.querySelector('.oranje2');
+var oranje3 = document.querySelector('.oranje3');
+var oranje4 = document.querySelector('.oranje4');
+var oranje5 = document.querySelector('.oranje5');
+var oranje6 = document.querySelector('.oranje6');
 
+var groen1 = document.querySelector('.groen1');
+var groen2 = document.querySelector('.groen2');
+var groen3 = document.querySelector('.groen3');
+var groen4 = document.querySelector('.groen4');
+var groen5 = document.querySelector('.groen5');
+var groen6 = document.querySelector('.groen6');
 
+var blauw1 = document.querySelector('.blauw1');
+var blauw2 = document.querySelector('.blauw2');
+var blauw3 = document.querySelector('.blauw3');
+var blauw4 = document.querySelector('.blauw4');
+var blauw5 = document.querySelector('.blauw5');
+var blauw6 = document.querySelector('.blauw6');
 
-knopOranje.addEventListener('click', function () {
-    rood1.classList.toggle("verstoppen");
-    rood2.classList.toggle("verstoppen");
-    rood3.classList.toggle("verstoppen");
-    rood4.classList.toggle("verstoppen");
-    rood5.classList.toggle("verstoppen");
-    rood6.classList.toggle("verstoppen");
-});
+var roze1 = document.querySelector('.roze1');
+var roze2 = document.querySelector('.roze2');
+var roze3 = document.querySelector('.roze3');
+var roze4 = document.querySelector('.roze4');
+var roze5 = document.querySelector('.roze5');
+var roze6 = document.querySelector('.roze6');
+
+var paars1 = document.querySelector('.paars1');
+var paars2 = document.querySelector('.paars2');
+var paars3 = document.querySelector('.paars3');
+var paars4 = document.querySelector('.paars4');
+var paars5 = document.querySelector('.paars5');
+var paars6 = document.querySelector('.paars6');
 
 knopRood.addEventListener('click', function () {
     oranje1.classList.toggle("verstoppen");
@@ -43,4 +56,69 @@ knopRood.addEventListener('click', function () {
     oranje4.classList.toggle("verstoppen");
     oranje5.classList.toggle("verstoppen");
     oranje6.classList.toggle("verstoppen");
+
+    groen1.classList.toggle("verstoppen");
+    groen2.classList.toggle("verstoppen");
+    groen3.classList.toggle("verstoppen");
+    groen4.classList.toggle("verstoppen");
+    groen5.classList.toggle("verstoppen");
+    groen6.classList.toggle("verstoppen");
+
+    blauw1.classList.toggle("verstoppen");
+    blauw2.classList.toggle("verstoppen");
+    blauw3.classList.toggle("verstoppen");
+    blauw4.classList.toggle("verstoppen");
+    blauw5.classList.toggle("verstoppen");
+    blauw6.classList.toggle("verstoppen");
+
+    roze1.classList.toggle("verstoppen");
+    roze2.classList.toggle("verstoppen");
+    roze3.classList.toggle("verstoppen");
+    roze4.classList.toggle("verstoppen");
+    roze5.classList.toggle("verstoppen");
+    roze6.classList.toggle("verstoppen");
+
+    paars1.classList.toggle("verstoppen");
+    paars2.classList.toggle("verstoppen");
+    paars3.classList.toggle("verstoppen");
+    paars4.classList.toggle("verstoppen");
+    paars5.classList.toggle("verstoppen");
+    paars6.classList.toggle("verstoppen");
+});
+
+knopOranje.addEventListener('click', function () {
+    rood1.classList.toggle("verstoppen");
+    rood2.classList.toggle("verstoppen");
+    rood3.classList.toggle("verstoppen");
+    rood4.classList.toggle("verstoppen");
+    rood5.classList.toggle("verstoppen");
+    rood6.classList.toggle("verstoppen");
+
+    groen1.classList.toggle("verstoppen");
+    groen2.classList.toggle("verstoppen");
+    groen3.classList.toggle("verstoppen");
+    groen4.classList.toggle("verstoppen");
+    groen5.classList.toggle("verstoppen");
+    groen6.classList.toggle("verstoppen");
+
+    blauw1.classList.toggle("verstoppen");
+    blauw2.classList.toggle("verstoppen");
+    blauw3.classList.toggle("verstoppen");
+    blauw4.classList.toggle("verstoppen");
+    blauw5.classList.toggle("verstoppen");
+    blauw6.classList.toggle("verstoppen");
+
+    roze1.classList.toggle("verstoppen");
+    roze2.classList.toggle("verstoppen");
+    roze3.classList.toggle("verstoppen");
+    roze4.classList.toggle("verstoppen");
+    roze5.classList.toggle("verstoppen");
+    roze6.classList.toggle("verstoppen");
+
+    paars1.classList.toggle("verstoppen");
+    paars2.classList.toggle("verstoppen");
+    paars3.classList.toggle("verstoppen");
+    paars4.classList.toggle("verstoppen");
+    paars5.classList.toggle("verstoppen");
+    paars6.classList.toggle("verstoppen");
 });
