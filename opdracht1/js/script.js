5ae7e2b66822b5a5c644f09a9caca07e00a78fe8
Lisa Oude Elferink <lisaoudeelferink@hotmail.com>
2020-05-14 15:21:04 +0200

    opdracht 1, versie 1.2

--- opdracht1/js/script.js
+++ opdracht1/js/script.js
@@ -3,36 +3,44 @@
 /*eslint 'no-console':0*/
 
 // VASTE VARIABELEN
-var buttonRood = document.querySelector('#roodknop');
-var buttonOranje = document.querySelector('#oranjeknop');
-var buttonGroen = document.querySelector('#groenknop');
-var buttonBlauw = document.querySelector('#blauwknop');
-var buttonRoze = document.querySelector('#rozeknop');
-var buttonPaars = document.querySelector('#paarsknop');
-
-var alleRodeFotos = document.querySelectorAll('.rood');
-var alleOranjeFotos = document.querySelectorAll('.oranje');
-var alleGroeneFotos = document.querySelectorAll('.groen');
-var alleBlauweFotos = document.querySelectorAll('.blauw');
-var alleRozeFotos = document.querySelectorAll('.roze');
-var allePaarseFotos = document.querySelectorAll('.paars');
+var knopRood = document.querySelector('#knoprood');
+var knopOranje = document.querySelector('#knoporanje');
+var knopGroen = document.querySelector('#knopgroen');
+var knopBlauw = document.querySelector('#knopblauw');
+var knopRoze = document.querySelector('#knoproze');
+var knopPaars = document.querySelector('#knoppaars');
 
 
-buttonRood.addEventListener('click', function () {
-    alleOranjeFotos.classList.toggle("verstoppen");
+var rood1 = document.querySelector('.rood1');
+var rood2 = document.querySelector('.rood2');
+var rood3 = document.querySelector('.rood3');
+var rood4 = document.querySelector('.rood4');
+var rood5 = document.querySelector('.rood5');
+var rood6 = document.querySelector('.rood6');
+
+var oranje1 = document.querySelector('.oranje1')
+var oranje2 = document.querySelector('.oranje2')
+var oranje3 = document.querySelector('.oranje3')
+var oranje4 = document.querySelector('.oranje4')
+var oranje5 = document.querySelector('.oranje5')
+var oranje6 = document.querySelector('.oranje6')
+
+
+
+knopOranje.addEventListener('click', function () {
+    rood1.classList.toggle("verstoppen");
+    rood2.classList.toggle("verstoppen");
+    rood3.classList.toggle("verstoppen");
+    rood4.classList.toggle("verstoppen");
+    rood5.classList.toggle("verstoppen");
+    rood6.classList.toggle("verstoppen");
 });
 
-
-console.log(alleRodeFotos);
-console.log(alleOranjeFotos);
-console.log(alleGroeneFotos);
-console.log(alleBlauweFotos);
-console.log(alleRozeFotos);
-console.log(allePaarseFotos);
-
-console.log(buttonRood);
-console.log(buttonOranje);
-console.log(buttonGroen);
-console.log(buttonBlauw);
-console.log(buttonRoze);
-console.log(buttonPaars);
+knopRood.addEventListener('click', function () {
+    oranje1.classList.toggle("verstoppen");
+    oranje2.classList.toggle("verstoppen");
+    oranje3.classList.toggle("verstoppen");
+    oranje4.classList.toggle("verstoppen");
+    oranje5.classList.toggle("verstoppen");
+    oranje6.classList.toggle("verstoppen");
+});
