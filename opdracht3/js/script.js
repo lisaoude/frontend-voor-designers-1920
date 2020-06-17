c6386e6465181e6bc8b4b916765fddc13d485076
Lisa Oude Elferink <lisaoudeelferink@hotmail.com>
2020-06-17 16:02:26 +0200

    Opdacht 3, Versie 2.3

--- opdracht3/js/script.js
+++ opdracht3/js/script.js
@@ -1,50 +1,47 @@
-// section declareren door deze aan te roepen van de HTML
 const section = document.querySelector('section');
+const button = document.getElementById('button');
+button.addEventListener('click', showBooks);
 
 
-// JSON bestand inladen mbv de link (6-12)
-let requestURL = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json';
-let request = new XMLHttpRequest();
+function showBooks(jsonObj) {
 
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function () {
+    let requestURL = 'https://raw.githubusercontent.com/benoitvallon/100-best-books/master/books.json';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
 
-    const movies = request.response;
+    request.responseType = 'json';
+    request.send();
 
-    // de functie showMovies zorgt ervoor dat inhoud wordt aangemaakt & in de HTML wordt weergegeven
-    showMovies(movies);
+    request.onload = function () {
+        const books = request.response;
+        showDetails(books);
+    }
 }
 
-
-// functie declareren, const 'details' aanmaken, welke data uit het JSON file haalt
-function showMovies(jsonObj) {
+function showDetails(jsonObj) {
     const details = jsonObj;
 
+    for (let i = 0; i < jsonObj.length; i++) {
 
-    // elementen aanmaken in de for loop, welke nodig zijn om de content te kunnen tonen. Hierdoor kunnen we door elke array 'loopen'.
-    for (let i = 0; i < details.length; i++) {
-
-        // article aanmaken waar de andere elementen in komen te staan
         const myArticle = document.createElement('article');
 
-        // elementen aanmaken (33-35)
         const title = document.createElement('h2');
-        const plot = document.createElement('p');
-        const cover = document.createElement('img');
+        const author = document.createElement('h3');
+        const year = document.createElement('h4');
+        const img = document.createElement('img');
 
-        // data voor de elementen ophalen (38-40)
         title.textContent = details[i].title;
-        plot.textContent = details[i].plot;
-        cover.src = details[i].cover;
+        author.textContent = details[i].author;
+        year.textContent = details[i].year;
+        img.src = details[i].imageLink;
 
-        // data van de elementen weergeven in het article (43-45)
+        img.setAttribute('class', 'cover');
+
         myArticle.appendChild(title);
-        myArticle.appendChild(plot);
-        myArticle.appendChild(cover);
+        myArticle.appendChild(author);
+        myArticle.appendChild(year);
+        myArticle.appendChild(img);
 
-        // element article weergeven, waar de rest vervolgens 'in' kan
         section.appendChild(myArticle);
     }
 }
