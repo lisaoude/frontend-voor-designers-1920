cc5568689bcb55d09462f1a714cece40cbfb2f00
Lisa Oude Elferink <lisaoudeelferink@hotmail.com>
2020-06-17 23:34:43 +0200

    Opdracht 3, Versie 3

--- opdracht3/js/script.js
+++ opdracht3/js/script.js
@@ -1,7 +1,9 @@
 const section = document.querySelector('section');
 const button = document.getElementById('button');
-button.addEventListener('click', showBooks);
+const buttonMore = document.getElementById('buttonMore');
 
+
+button.addEventListener('click', showBooks);
 
 function showBooks(jsonObj) {
 
@@ -18,12 +20,10 @@ function showBooks(jsonObj) {
     }
 }
 
-
-
 function showDetails(jsonObj) {
     const details = jsonObj;
 
-    for (let i = 0; i < jsonObj.length; i++) {
+    for (let i = 0; i < 9; i++) {
 
         const myArticle = document.createElement('article');
 
@@ -46,15 +46,69 @@ function showDetails(jsonObj) {
     }
 }
 
+
 button.addEventListener('click', removePlaceholder);
 
 function removePlaceholder() {
     var myDiv = document.getElementById('placeholder1');
-    myDiv.classList.toggle('remove');
+    myDiv.classList.add('remove');
 
     var myDiv = document.getElementById('placeholder2');
-    myDiv.classList.toggle('remove');
+    myDiv.classList.add('remove');
 
     var myDiv = document.getElementById('placeholder3');
-    myDiv.classList.toggle('remove');
+    myDiv.classList.add('remove');
+}
+
+
+
+buttonMore.addEventListener('click', showMore);
+
+function showMore(jsonObj) {
+
+    let requestURL = 'https://raw.githubusercontent.com/benoitvallon/100-best-books/master/books.json';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function () {
+        const books = request.response;
+        showMoreBooks(books);
+    }
+}
+
+function showMoreBooks(jsonObj) {
+    const details = jsonObj;
+
+    for (let i = 9; i < 18; i++) {
+
+        const myArticle = document.createElement('article');
+
+        const title = document.createElement('h1');
+        const author = document.createElement('h2');
+        const year = document.createElement('h3');
+        const myImg = document.createElement('img');
+
+        title.textContent = details[i].title;
+        author.textContent = details[i].author;
+        year.textContent = details[i].year;
+        myImg.src = details[i].imageLink;
+
+        myArticle.appendChild(title);
+        myArticle.appendChild(author);
+        myArticle.appendChild(year);
+        myArticle.appendChild(myImg);
+
+        section.appendChild(myArticle);
+    }
+}
+
+buttonMore.addEventListener('click', removeButton);
+
+function removeButton() {
+    var myDiv = document.getElementById('buttonMore');
+    myDiv.classList.add('removePLS');
+
 }
